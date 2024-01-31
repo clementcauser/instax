abaf03e49686c8f9639b364f7645e016baef32fd
Clement Causer <clement.causer@gmail.com>
2024-01-31 13:54:06 +0100

    implementing blocs

---
+++ lib/blocs/authentication/authentication_bloc.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+import 'package:user_repository/user_repository.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+  late final StreamSubscription<User?> _userSubscription;
+
+  AuthenticationBloc(UserRepository myUserRepository)
+      : userRepository = myUserRepository,
+        super(const AuthenticationState.unknown()) {
+    _userSubscription = userRepository.user.listen(
+      (authUser) {
+        add(AuthenticationUserChanged(authUser));
+      },
+    );
+
+    on<AuthenticationUserChanged>((event, emit) {
+      try {
+        if (event.user != null) {
+          emit(AuthenticationState.authenticated(event.user!));
+        } else {
+          emit(const AuthenticationState.unauthenticated());
+        }
+      } catch (e) {
+        log(e.toString());
+        rethrow;
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _userSubscription.cancel();
+
+    return super.close();
+  }
+}
