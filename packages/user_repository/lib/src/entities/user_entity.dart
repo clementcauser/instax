194d73da5d2a3184abb702e96a19a34eca60fff9
Clement Causer <clement.causer@gmail.com>
2024-01-31 11:08:58 +0100

    setup structure complete user entity and user model

---
+++ packages/user_repository/lib/src/entities/user_entity.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUserEntity(
+      {required this.id,
+      required this.email,
+      required this.name,
+      this.picture});
+
+  Map<String, Object?> toDocument() {
+    return {'id': id, 'email': email, 'name': name, 'picture': picture};
+  }
+
+  MyUserEntity fromDocument(Map<String, dynamic> document) {
+    return MyUserEntity(
+        id: document['id'],
+        email: document['email'],
+        name: document['name'],
+        picture: document['picture']);
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+}
